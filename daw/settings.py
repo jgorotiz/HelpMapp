90bc5180d81c7aca014ca4925bc241da5de01556
Ma.Belen <mguarand@espol.edu.ec>
2017-07-22 23:26:05 -0500

    tratando de desplegar en heroku otra vez

--- daw/settings.py
+++ daw/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
