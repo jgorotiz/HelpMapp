6230adaa92d184cfdf3479a44d71fa7c0f6f204f
flayedra@espol.edu.ec <flayedra@espol.edu.ec>
2017-07-30 18:22:32 -0500

    se ve mejor el  index

--- helpmapp/urls.py
+++ helpmapp/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
 from . import views
 urlpatterns = [
-        url(r'^$', views.show_index),
-        # url(r'^$', views.get_name),
+        url(r'^$', views.show_index, name='show_index'),
+         url(r'^lol$', views.get_name),
 
 #        url(r'^thanks/', views.show_name),
         url(r'^thanks/', views.show_name),
