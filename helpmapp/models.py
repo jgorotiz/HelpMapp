442b7acea5708dd5736b2ed0e532e1c84a90e8f0
jgorotiz <jgorotiz@espol.edu.ec>
2017-07-30 16:59:11 -0500

    Modelos arreglados

--- helpmapp/models.py
+++ helpmapp/models.py
@@ -1,26 +1,26 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 
 class HelpMapper(models.Model):
 	id_HelpMapper = models.CharField( primary_key=True,max_length=16)
-    nombre_usuario = models.CharField(max_length=12)
-    contrasenia = models.CharField(min_length=6,max_length=15)
-    correo = models.EmailField(max_length=100)
-    estado = models.IntegerField(max_digits=1)
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
-    tipo_sangre = models.CharField(max_length=5)
-    cedula = models.CharField(min_length=10,max_length=10)
-    telefono = models.CharField(max_length=10)
-    sexo = models.CharField(max_length=10)
+	nombre_usuario = models.CharField(max_length=12)
+	contrasena = models.CharField(max_length=15)
+	correo = models.EmailField(max_length=100)
+	estado = models.IntegerField()
+	nombre = models.CharField(max_length=100)
+	apellido = models.CharField(max_length=100)
+	tipo_sangre = models.CharField(max_length=5)
+	cedula = models.CharField( max_length=10)
+	telefono = models.CharField(max_length=10)
+	sexo = models.CharField(max_length=10)
 
-    def save(self,*args, **kwargs):
-        super(HelpMapper,self).save(*args, **kwargs)
+	def save(self,*args, **kwargs):
+		super(HelpMapper,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.HelpMapper
+	def __str__(self):
+		return self.HelpMapper
 
 class HabilidadHelpMapper(models.Model):
 	id_HabilidadHelpMapper = models.CharField( primary_key=True,max_length=16)
@@ -28,49 +28,49 @@ class HabilidadHelpMapper(models.Model):
 	id_Hablidad = models.CharField(max_length=16)
 
 	def save(self,*args, **kwargs):
-        super(HabilidadHelpMapper,self).save(*args, **kwargs)
+		super(HabilidadHelpMapper,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.HabilidadHelpMapper
+	def __str__(self):
+		return self.HabilidadHelpMapper
 
 class Habilidad(models.Model):
 	id_Habilidad = models.CharField( primary_key=True,max_length=16)
 	nombre_habilidad = models.CharField(max_length=100)
 
 	def save(self,*args, **kwargs):
-        super(Habilidad,self).save(*args, **kwargs)
+		super(Habilidad,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.Habilidad
+	def __str__(self):
+		return self.Habilidad
 
 class cambioInventario(models.Model):
 	id_CambioInventario = models.CharField(primary_key=True,max_length=16)
-	tipo = models.IntegerField(max_digits=1) #(-1) correccion  (0) envio  (1) ingreso
+	tipo = models.IntegerField() #(-1) correccion  (0) envio  (1) ingreso
 	cantidad = models.DecimalField(default=0,max_digits=6,decimal_places=2)
 	id_producto = models.CharField(max_length=16)
 	fecha = models.DateField(default=datetime.date.today)
 
 	def save(self,*args, **kwargs):
-        super(cambioInventario,self).save(*args, **kwargs)
+		super(cambioInventario,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.cambioInventario
+	def __str__(self):
+		return self.cambioInventario
 
 class Administrador(models.Model):
 	idAdministrador = models.CharField(primary_key=True,max_length=16)
 	nombreUsuario = models.CharField(max_length=12)
-	contrasena = models.CharField(min_length=6, max_length=15)
+	contrasena = models.CharField( max_length=15)
 	correo = models.EmailField(max_length=254)
-	tipo = models.IntegerField(max_digits=1)
+	tipo = models.IntegerField()
 	#revisar FK
 	idCentro = models.CharField(max_length=16)
-	estado = models.IntegerField(max_digits=1)
+	estado = models.IntegerField()
 
-    def save(self,*args, **kwargs):
-        super(Administrador,self).save(*args, **kwargs)
+	def save(self,*args, **kwargs):
+		super(Administrador,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.idAdministrador
+	def __str__(self):
+		return self.idAdministrador
 
 
 class CentroDeAcopio(models.Model):
@@ -81,13 +81,13 @@ class CentroDeAcopio(models.Model):
 	longitud = models.DecimalField(default=0,max_digits=15,decimal_places=10)
 	provincia = models.CharField(max_length=30)
 	canton = models.CharField(max_length=30)
-	estado = models.IntegerField(max_digits=1)
+	estado = models.IntegerField()
 
-    def save(self,*args, **kwargs):
-        super(CentroDeAcopio,self).save(*args, **kwargs)
+	def save(self,*args, **kwargs):
+		super(CentroDeAcopio,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.idCentro
+	def __str__(self):
+		return self.idCentro
 
 class Producto(models.Model):
 	idProducto = models.CharField(primary_key=True,max_length=16)	
@@ -98,20 +98,20 @@ class Producto(models.Model):
 	#revisar FK
 	idCentro = models.CharField(max_length=16)
 
-    def save(self,*args, **kwargs):
-        super(Producto,self).save(*args, **kwargs)
+	def save(self,*args, **kwargs):
+		super(Producto,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.idProducto
+	def __str__(self):
+		return self.idProducto
 
 class Categoria(models.Model):
 	idCategoria = models.CharField(primary_key=True,max_length=16)	
 	nombreCategoria = models.CharField(max_length=30)
 	unidad = models.CharField(max_length=20)
 
-    def save(self,*args, **kwargs):
-        super(Categoria,self).save(*args, **kwargs)
+	def save(self,*args, **kwargs):
+		super(Categoria,self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.idCategoria
+	def __str__(self):
+		return self.idCategoria
 
