f0be5d0df88df357356150c66f4610430bfde1ef
Jonathan <jgorotiz@espol.edu.ec>
2017-08-20 18:47:09 -0500

    Arreglando markets

--- helpmapp/urls.py
+++ helpmapp/urls.py
@@ -30,7 +30,8 @@ urlpatterns = [
         url(r'^crearAdmin/', views.mostrar_crearAdministrador,name="mostrar_crearAdministrador"),
         url(r'^verCentro/', views.mostrar_verCentro,name="mostrar_verCentro"),
         url(r'^admin/recuperarCuenta', views.mostrar_recuperarCuenta,name="mostrar_recuperarCuenta"),
-        url(r'^recoverPass/', views.recoverPass,name="recoverPass"),
+
+        url(r'^recovery/', views.recovery,name="recovery"),
         
         url(r'^cerrarSesion/', views.cerrarSesion,name="cerrarSesion"),
 
