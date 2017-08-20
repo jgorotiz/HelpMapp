ded75604b6f28c5c233002eca479168e901b73d1
gaalocastillo <gadacast@espol.edu.ec>
2017-08-19 19:07:00 -0500

    Formulario de registrar voluntario

--- helpmapp/forms.py
+++ helpmapp/forms.py
@@ -1,19 +1,11 @@
 from django import forms
 from .models import *
 
-class VoluntaryForm(forms.Form):
-    nombre = forms.CharField(label='Nombres')
-    apellido=forms.CharField(label='Apellidos')
-    cedula=forms.CharField(label='Cedula')
-    tipo_sangre=forms.ChoiceField(label="Tipo de sangre")
-    GENDER_CHOICES = (('M', 'Masculino'),('F', 'Femenino'))
-    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect())
-    telefono=forms.CharField(label='Telefono')
-    email=forms.EmailField(label="Correo")
-    suscribe = forms.BooleanField(required=False)
-    auxilios=forms.BooleanField(required=False)
-    culinarias=forms.BooleanField(required=False)
-    trabajo_campo=forms.BooleanField(required=False)
+class HelpMapperForm(forms.Form):
+    class Meta:
+        model = HelpMapper
+        fields = ('nombre', 'apellido', 'nombreUsuario', 'contrasena', 'sexo', 'cedula',
+                    'tipoSangre', 'telefono', 'correo', 'habilidad')
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Nombre de usuario', max_length=16)
@@ -21,3 +13,4 @@ class LoginForm(forms.Form):
    
 class RecoverForm(forms.Form):
     correo = forms.CharField(label='Correo Electrónico', max_length=100)
+    
